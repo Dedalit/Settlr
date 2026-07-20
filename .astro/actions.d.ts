e84da1ab95852f8f240f1cd1b701b4057db39fc9
Dav Co <iksvdx@zohomail.eu>
2026-07-20 17:58:00 +0200

    fix: added middleware to protect paths for signed-in users, upgraded to new ver. of Astro and React

---
+++ .astro/actions.d.ts
@@ -0,0 +1,3 @@
+declare module "astro:actions" {
+		export const actions: typeof import("/Users/ween/Library/Mobile Documents/com~apple~CloudDocs/Personal ⭐️/Progetti 📬/SETTLR/src/actions/index.ts")["server"];
+}
