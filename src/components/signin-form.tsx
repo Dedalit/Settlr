0b781c206beab7682f52d2db6cfb65b6efbc813f
Dav Co <iksvdx@zohomail.eu>
2026-07-23 00:48:39 +0200

    update: FallingReceipts component implementation, pages cleanup

--- src/components/signin-form.tsx
+++ src/components/signin-form.tsx
@@ -16,7 +16,7 @@ import {
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 
-export function LoginForm({
+export function SignInForm({
   className,
   onSuccess,
   ...props
