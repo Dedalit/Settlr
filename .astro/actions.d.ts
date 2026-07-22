753b1816f198a68c999b70764ae894cde2ee8ef8
Dav Co <iksvdx@zohomail.eu>
2026-07-22 21:50:56 +0200

    fix: update gitignore

--- .astro/actions.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "astro:actions" {
-		export const actions: typeof import("/Users/ween/Library/Mobile Documents/com~apple~CloudDocs/Personal ⭐️/Progetti 📬/CODE/Settlr/src/actions/index.ts")["server"];
-}
